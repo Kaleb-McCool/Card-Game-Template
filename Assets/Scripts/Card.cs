5e21e36545ec6d7d43fc715b420ea971c841021b
Alex717MacKay <144173348+Alex717MacKay@users.noreply.github.com>
2024-04-02 13:10:37 -0700

    card work :)

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -8,30 +8,26 @@ public class Card : MonoBehaviour
 {
     public Card_data data;
 
-    public string card_name;
-    public string description;
-    public Sprite Image1;
-    public Sprite Image2;
-    public Sprite MEME;
-    public TextMeshProUGUI Card_Name;
+    
     public TextMeshProUGUI descriptionText;
     public TextMeshProUGUI number1;
     public TextMeshProUGUI number2;
     public Image spriteImage;
+    public Image spriteImage2;
+    public Image memeImage;
         
 
     // Start is called before the first frame update
     void Start()
     {
-        card_name = data.card_name;
-        description = data.description;
-        Image1 = data.Image1;
-        Image2 = data.Image2;
-        MEME = data.MEME;
-        descriptionText.text = description;
-        number1.text = number1.ToString();
-        number2.text = number2.ToString();
-        spriteImage.sprite = MEME;
+        descriptionText.text = data.description;
+        spriteImage.sprite = data.Image1;
+        spriteImage2.sprite= data.Image1;
+        memeImage.sprite = data.MEME;
+        
+        number1.text = data.number1;
+        number2.text = data.number1;
+        
 
     }
 
--- Assets/Scripts/Card_data.cs
+++ Assets/Scripts/Card_data.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Card_data", menuName = "Cards/Card_data", order = 1)]
 public class Card_data : ScriptableObject
 {
-    public string card_name;
     public Sprite Image1;
-    public Sprite Image2;
     public string description;
-    public int number1;
-    public int number2;
+    public string number1;
     public Sprite MEME;
 
 }
